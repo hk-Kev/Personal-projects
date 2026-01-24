dcbd47946db70ce63aadce7818f0e13bfa479c6b
HIRWA KAYIHURA Kevin <hirwakayihurakevin@gmail.com>
2026-01-24 20:08:58 +0200

    Password Generator

---
+++ Scrimba/Solo Projects/Password Generator/index.js
@@ -0,0 +1,115 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let firstPass = document.getElementById("first-pass");
+let secondPass = document.getElementById("second-pass");
+let firstPassword = [];
+let secondPassword = [];
+
+function numberGenerator() {
+  return Math.floor(Math.random() * characters.length) + 1;
+}
+
+function passwordGenerator() {
+  firstPass.style.color = "#4adf86";
+  secondPass.style.color = "#4adf86";
+  firstPassword = [];
+  secondPassword = [];
+  for (let i = 0; i < 15; i++) {
+    firstPassword.push(characters[numberGenerator()]);
+    secondPassword.push(characters[numberGenerator()]);
+  }
+  secondPass.textContent = firstPassword.join("");
+  firstPass.textContent = secondPassword.join("");
+}
